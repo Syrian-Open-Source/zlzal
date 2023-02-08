021645858dfd77772847cc91d236755f24e76c0d
Mahmoud Mahmoud <54012843+Mahmoud217TR@users.noreply.github.com>
2023-02-08 22:24:33 +0300

    refactoring views

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>زلزال</title>
+    <title>@yield('title','زلزال')</title>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <!-- Load Leaflet from CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.3/leaflet.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.3/leaflet.js"></script>
+    <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css" integrity="sha384-JvExCACAZcHNJEc7156QaHXTnQL3hQBixvj5RV5buE7vgnNEzzskDtx9NQ4p6BJe" crossorigin="anonymous">
 
     <!-- Load geocoding plugin after Leaflet -->
     <link rel="stylesheet" href="https://maps.locationiq.com/v2/libs/leaflet-geocoder/1.9.6/leaflet-geocoder-locationiq.min.css">
     <script src="https://maps.locationiq.com/v2/libs/leaflet-geocoder/1.9.6/leaflet-geocoder-locationiq.min.js"></script>
+    
+    @stack('style')
 </head>
 <body>
 <main>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,91 @@
-<!DOCTYPE html>
-<html lang="ar" dir="rtl">
-<head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <link rel="stylesheet" href="https://cdn.rtlcss.com/bootstrap/v4.5.3/css/bootstrap.min.css" integrity="sha384-JvExCACAZcHNJEc7156QaHXTnQL3hQBixvj5RV5buE7vgnNEzzskDtx9NQ4p6BJe" crossorigin="anonymous">
+@extends('master')
+@section('content')
+    <div class="d-flex justify-content-center align-items-center form" style="height: 100vh; width: 100%">
+        <form class="p-5" action="{{route('cases.store')}}" method="POST">
+            @csrf
+            <h2 class="text-center mb-5">زلزال</h2>
+            <p class="text-center mb-3">زلزال هو موقع بسيط يساعد على تحديد وتنظيم بيانات تتعلق بالاماكن والمناطق المنكوبة والفرق العاملة على ايصال المساعدات لها في سوريا</p>
+            <p class="text-center mb-3">يرجى تحديد المعلومات بدقة كبيرة لمساعدتنا في ربط المعلومات مع الجهات المختصة وتحديدها على الخريطة بدقة</p>
+            <h5 class="text-center"><a href="/map">لرؤية الخريطة من هنا</a> </h5>
+        @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+            <div class="container">
+                @if (session('success'))
+                    <div class="alert alert-success">
+                        {{ session('success') }}
+                    </div>
+                @endif
+            <div class="form-group my-4">
+                <label for="case">حدد نوع الحالة <span style="color: red">* </span></label>
+                <select name="type" class="form-control" id="cases" required>
+                    <option value="1" selected>انقاض، اشخاص عالقين تحت الانقاض.</option>
+                    <option value="2">مراكز يتوفر فيها حاجات اساسية مثل الطعام والشراب</option>
+                    <option value="3">مراكز يتوفر فيها ملابس وتدفئة</option>
+                    <option value="4">فريق استجابة، فرق تطوعية</option>
+                    <option value="5">مراكز ايواء</option>
+                    <option value="6">مراكز يتوفر فيها مواصلات</option>
+                    <option value="7">مراكز أمنة</option>
+                    <option value="8">مراكز غسيل كلية</option>
+                    <option value="9">مراكز ونقاط طبية</option>
+                    <option value="10">تقديم المساعدة والتطوع</option>
+                    <option value="11">أخرى</option>
+                </select>
+            </div>
 
-    <title>زلزال</title>
+            <div class="form-group">
+                <label for="name">الاسم الثلاثي متطوع او طبيب او مهندس\اسم الجهة\اسم الفريق\اسم المركز <span style="color: red">* </span></label>
+                <input type="text" required class="form-control" value="{{old('name')}}" name="name" id="name">
+            </div>
+            <div class="form-group">
+                <label for="name">رقم للتواصل\ اي وسيلة تواصل مساعدة <span style="color: red">* </span></label>
+                <input type="text" required class="form-control" value="{{old('phone')}}" name="phone" id="name">
+            </div>
+
+            <div class="form-group">
+                <label for="case-description">وصف الحالة\وصف البناء\ وصف امكانيات الفريق او نطاق عمله\ وصف طاقة المراكز الطبية   </label>
+                <textarea class="form-control" name="description"  id="case-description" rows="3">{{old('description')}}</textarea>
+            </div>
+            <div class="form-group">
+                <label for="case">حالة النقاط الطبية (خاص لنوع الحالة نقطة طبية)</label>
+                <select name="medical_point_status" class="form-control" id="cases">
+                    <option value="1" selected>فعالة</option>
+                    <option value="2">جزئي</option>
+                    <option value="0">غير فعالة</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="city"> المدينة<span style="color: red">* </span></label>
+                <input type="text" name="city" required value="{{old('city')}}" class="form-control" id="city">
+            </div>
+
+            <div class="form-group">
+                <label for="area"> المنطقة<span style="color: red">* </span></label>
+                <input type="text" name="area" required value="{{old('area')}}" class="form-control" id="area">
+            </div>
+
+            <div class="form-group">
+                <label for="street"> الشارع <span style="color: red">* </span></label>
+                <input type="text" name="street" required value="{{old('street')}}" class="form-control" id="street">
+            </div>
+
+            <div class="form-group">
+                <label for="additional-data">أي بيانات اضافية</label>
+                <textarea class="form-control"  name="more_info" id="additional-data" rows="3">{{old('more_info')}}</textarea>
+            </div>
+            <button type="submit" class="btn btn-primary btn-block">ارسال</button>
+        </form>
+    </div>
+@endsection
+
+@push('style')
     <style>
         body{
             direction: rtl;
@@ -24,92 +103,4 @@
             font-size: 15px;
         }
     </style>
-</head>
-<body>
-<div class="d-flex justify-content-center align-items-center form" style="height: 100vh; width: 100%">
-    <form class="p-5" action="{{route('cases.store')}}" method="POST">
-        @csrf
-        <h2 class="text-center mb-5">زلزال</h2>
-        <p class="text-center mb-3">زلزال هو موقع بسيط يساعد على تحديد وتنظيم بيانات تتعلق بالاماكن والمناطق المنكوبة والفرق العاملة على ايصال المساعدات لها في سوريا</p>
-        <p class="text-center mb-3">يرجى تحديد المعلومات بدقة كبيرة لمساعدتنا في ربط المعلومات مع الجهات المختصة وتحديدها على الخريطة بدقة</p>
-        <h5 class="text-center"><a href="/map">لرؤية الخريطة من هنا</a> </h5>
-    @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <div class="container">
-            @if (session('success'))
-                <div class="alert alert-success">
-                    {{ session('success') }}
-                </div>
-            @endif
-        <div class="form-group my-4">
-            <label for="case">حدد نوع الحالة <span style="color: red">* </span></label>
-            <select name="type" class="form-control" id="cases" required>
-                <option value="1" selected>انقاض، اشخاص عالقين تحت الانقاض.</option>
-                <option value="2">مراكز يتوفر فيها حاجات اساسية مثل الطعام والشراب</option>
-                <option value="3">مراكز يتوفر فيها ملابس وتدفئة</option>
-                <option value="4">فريق استجابة، فرق تطوعية</option>
-                <option value="5">مراكز ايواء</option>
-                <option value="6">مراكز يتوفر فيها مواصلات</option>
-                <option value="7">مراكز أمنة</option>
-                <option value="8">مراكز غسيل كلية</option>
-                <option value="9">مراكز ونقاط طبية</option>
-                <option value="10">تقديم المساعدة والتطوع</option>
-                <option value="11">أخرى</option>
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="name">الاسم الثلاثي متطوع او طبيب او مهندس\اسم الجهة\اسم الفريق\اسم المركز <span style="color: red">* </span></label>
-            <input type="text" required class="form-control" value="{{old('name')}}" name="name" id="name">
-        </div>
-        <div class="form-group">
-            <label for="name">رقم للتواصل\ اي وسيلة تواصل مساعدة <span style="color: red">* </span></label>
-            <input type="text" required class="form-control" value="{{old('phone')}}" name="phone" id="name">
-        </div>
-
-        <div class="form-group">
-            <label for="case-description">وصف الحالة\وصف البناء\ وصف امكانيات الفريق او نطاق عمله\ وصف طاقة المراكز الطبية   </label>
-            <textarea class="form-control" name="description"  id="case-description" rows="3">{{old('description')}}</textarea>
-        </div>
-        <div class="form-group">
-            <label for="case">حالة النقاط الطبية (خاص لنوع الحالة نقطة طبية)</label>
-            <select name="medical_point_status" class="form-control" id="cases">
-                <option value="1" selected>فعالة</option>
-                <option value="2">جزئي</option>
-                <option value="0">غير فعالة</option>
-            </select>
-        </div>
-
-        <div class="form-group">
-            <label for="city"> المدينة<span style="color: red">* </span></label>
-            <input type="text" name="city" required value="{{old('city')}}" class="form-control" id="city">
-        </div>
-
-        <div class="form-group">
-            <label for="area"> المنطقة<span style="color: red">* </span></label>
-            <input type="text" name="area" required value="{{old('area')}}" class="form-control" id="area">
-        </div>
-
-        <div class="form-group">
-            <label for="street"> الشارع <span style="color: red">* </span></label>
-            <input type="text" name="street" required value="{{old('street')}}" class="form-control" id="street">
-        </div>
-
-        <div class="form-group">
-            <label for="additional-data">أي بيانات اضافية</label>
-            <textarea class="form-control"  name="more_info" id="additional-data" rows="3">{{old('more_info')}}</textarea>
-        </div>
-        <button type="submit" class="btn btn-primary btn-block">ارسال</button>
-    </form>
-</div>
-
-</body>
-
-</html>
+@endpush
